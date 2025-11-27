5026d0cb2f942c98b13af27b5987b7d14cd66378
Samuel Jorge <samuca948@gmail.com>
2025-11-27 16:54:37 -0300

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_QnCVZR4scg_Q3X6_SXlfMzF_ENzCS-s",
+  authDomain: "pdvpastel.firebaseapp.com",
+  projectId: "pdvpastel",
+  storageBucket: "pdvpastel.firebasestorage.app",
+  messagingSenderId: "482208869366",
+  appId: "1:482208869366:web:d83dc72f7daa2d8cf535d7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
